50dec4ce5d2dfd38ba14428a262df1348b4dff16
Leonid Zychkov <buldog52@gmail.com>
2021-05-12 22:06:55 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+window.$ = window.jQuery = require('jquery');
+const datepicker = require('js-datepicker');
+
+import './components/form-styler/form-styler';
---
+++ src/components/form-styler/form-styler.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+window.$ = window.jQuery = require('jquery');
+require('jquery-form-styler');
+
+$('.trip-qty__item-number').styler();
