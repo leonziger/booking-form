21dd86b71b46fd084f5c40685a848dcf65b09cd0
Leonid Zychkov <buldog52@gmail.com>
2021-05-12 22:56:30 +0300

    Almost ready

--- src/app.js
+++ src/app.js
@@ -2,3 +2,7 @@ window.$ = window.jQuery = require('jquery');
 const datepicker = require('js-datepicker');
 
 import './components/form-styler/form-styler';
+
+$('button').click(function() {
+    return false;
+});
